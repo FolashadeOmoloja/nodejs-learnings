64b8b1ad49ef0a1eebd4f465424195c92896c76f
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-24 16:39:21 +0100

    test.js changed

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
-// . How to export ./
-// METHOD 1
-const add = (a,b) => a + b
-const subtract = (a,b) => a - b
-const multiply = (a,b) => a * b
-const divide= (a,b) => a / b
+
 
 module.exports = {add, subtract, multiply, divide}
 

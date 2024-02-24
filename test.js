d62c1b4db55bd2bdd50d7abef5005ccd20e14349
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-24 16:35:37 +0100

    test.js added

--- test.js
+++ test.js
@@ -1,33 +1,50 @@
-//. How to export ./
-//METHOD 1
-// const add = (a,b) => a + b
-// const subtract = (a,b) => a - b
-// const multiply = (a,b) => a * b
-// const divide= (a,b) => a / b
+console.log('sup world')
+//use node server to run it
+//there is a global object instead of window object
+// console.log(global)
 
-// module.exports = {add, subtract, multiply, divide}
+//has common core modluless that we will explore
+//commonjs modules used instead of es6 modules 
+//imports
+const os = require("os")
+const path =  require('path')
+const math = require('./math')
+const {subtract, multiply, divide, add} = require('./math')
 
-//Method 2
-exports.add = (a,b) => a + b
-exports.subtract = (a,b) => a - b
-exports.multiply = (a,b) => a * b
-exports.divide= (a,b) => a / b
+console.log(math.add(2,5))
+console.log(subtract(10,5))
+console.log(multiply(10,5))
+console.log(divide(10,5))
 
-// you can either use module.exports or just straight away export
 
-//. How to export ./
-//METHOD 1
-// const add = (a,b) => a + b
-// const subtract = (a,b) => a - b
-// const multiply = (a,b) => a * b
-// const divide= (a,b) => a / b
 
-// module.exports = {add, subtract, multiply, divide}
 
-//Method 2
-exports.add = (a,b) => a + b
-exports.subtract = (a,b) => a - b
-exports.multiply = (a,b) => a * b
-exports.divide= (a,b) => a / b
 
-// you can either use module.exports or just straight away export
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
+
+// console.log(__dirname)
+// console.log(__filename)
+
+//* filename and diretory name and extension name *//
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename)) //extension name
+// //* results *//
+// C:\Users\HP\Desktop\front end dev\nodejs -folder
+// C:\Users\HP\Desktop\front end dev\nodejs\server.js -file
+// C:\Users\HP\Desktop\front end dev\nodejs
+// server.js
+// .js -language extension
+
+// console.log(path.parse(__filename))
+//Result
+
+// {
+//     root: 'C:\\',
+//     dir: 'C:\\Users\\HP\\Desktop\\front end dev\\nodejs',
+//     base: 'server.js',
+//     ext: '.js',
+//     name: 'server'
+//   }  you get everthing

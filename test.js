f75ed13c96b538d089de090e799a8147e69258fc
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-24 16:39:08 +0100

    test.js changed

--- test.js
+++ test.js
@@ -1,33 +1,19 @@
-//. How to export ./
-//METHOD 1
-// const add = (a,b) => a + b
-// const subtract = (a,b) => a - b
-// const multiply = (a,b) => a * b
-// const divide= (a,b) => a / b
+// . How to export ./
+// METHOD 1
+const add = (a,b) => a + b
+const subtract = (a,b) => a - b
+const multiply = (a,b) => a * b
+const divide= (a,b) => a / b
 
-// module.exports = {add, subtract, multiply, divide}
-
-//Method 2
-exports.add = (a,b) => a + b
-exports.subtract = (a,b) => a - b
-exports.multiply = (a,b) => a * b
-exports.divide= (a,b) => a / b
-
-// you can either use module.exports or just straight away export
-
-//. How to export ./
-//METHOD 1
-// const add = (a,b) => a + b
-// const subtract = (a,b) => a - b
-// const multiply = (a,b) => a * b
-// const divide= (a,b) => a / b
-
-// module.exports = {add, subtract, multiply, divide}
+module.exports = {add, subtract, multiply, divide}
 
 //Method 2
-exports.add = (a,b) => a + b
-exports.subtract = (a,b) => a - b
-exports.multiply = (a,b) => a * b
+exports.add = (c,b) => a + b
+exports.subtract = (a,d) => a - b
+exports.multiply = (a,v) => a * b
 exports.divide= (a,b) => a / b
+exports.complicate= (a,b,c,d,e) => a / b*c*d+e
+exports.divide= (a,b) => a / b
+
 
 // you can either use module.exports or just straight away export
